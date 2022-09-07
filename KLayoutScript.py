67c84694c171be1bfe40e52e9058cc93f639a677
Anders Christian Mathisen <anderscm@mit.edu>
2022-09-07 16:36:46 +0200

    Initial Klayoutscript template

---
+++ KLayoutScript.py
@@ -0,0 +1,11 @@
+import pya
+
+# Enter your Python code here ..
+
+layout = pya.Layout()
+
+top = layout.create_cell("TOP")
+l1 = layout.layer(1, 0)
+top.shapes(l1).insert(pya.Box(0, 0, 10, 2000))
+
+layout.write("/Users/anders/Downloads/Single_LED.GDS")
